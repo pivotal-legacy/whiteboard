4255b139269fdbad4fe1fda9777a8cb3e21203ea
Peter Iskandar <piskandar@pivotal.io>
2017-08-21 17:19:00 -0400

    updated concourse pipeline to use provided SENTRY_DSN env variable
    Signed-off-by: Drew Archibald <darchibald@pivotal.io>

--- concourse/tasks/deploy/deploy.sh
+++ concourse/tasks/deploy/deploy.sh
@@ -6,6 +6,7 @@ cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE -a https://api.
 cf set-env whiteboard OKTA_SSO_TARGET_URL $OKTA_SSO_TARGET_URL
 cf set-env whiteboard OKTA_CERT_FINGERPRINT $OKTA_CERT_FINGERPRINT
 cf set-env whiteboard IP_WHITELIST $IP_WHITELIST
+cf set-env whiteboard SENTRY_DSN $SENTRY_DSN
 cd whiteboard
 bundle install
 bundle exec rake SPACE=$CF_SPACE cf:deploy:$ENVIRONMENT
