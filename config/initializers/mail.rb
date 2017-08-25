a1937e637951623612af2e8cfd51b5064cdf2e59
Peter Iskandar <piskandar@pivotal.io>
2017-08-25 11:23:04 -0400

    updated mail logic to check for SMTP_RELAY before checking vcap_services. updated evaporator git link to be https
    Signed-off-by: Drew Archibald <darchibald@pivotal.io>

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,19 +1,31 @@
 require 'json'
 
-if ENV['VCAP_SERVICES'].present?
+if ENV['SMTP_RELAY'].present?
 
-  vcap_services = JSON.parse(ENV['VCAP_SERVICES'])
-  # After starting your cloud foundry app run:
-  # cf files [app-name] logs/env.log to see the VCAP_SERVICES vars
-  credentials = vcap_services["sendgrid"][0]['credentials']
+  smtp_relay = ENV['SMTP_RELAY']
 
   ActionMailer::Base.smtp_settings = {
-    :address => credentials['hostname'],
-    :port => '587',
-    :authentication => :plain,
-    :user_name => credentials['username'],
-    :password => credentials['password'],
-    :domain => 'cfapps.io'
+      :address => smtp_relay,
   }
 
+elsif ENV['VCAP_SERVICES'].present?
+
+  vcap_services = JSON.parse(ENV['VCAP_SERVICES'])
+
+  if vcap_services.include? "sendgrid"
+    # After starting your cloud foundry app run:
+    # cf files [app-name] logs/env.log to see the VCAP_SERVICES vars
+    credentials = vcap_services["sendgrid"][0]['credentials']
+
+    ActionMailer::Base.smtp_settings = {
+        :address => credentials['hostname'],
+        :port => '587',
+        :authentication => :plain,
+        :user_name => credentials['username'],
+        :password => credentials['password'],
+        :domain => 'cfapps.io'
+    }
+  else
+    Rails.logger.info "Send grid not present, mailer not configured"
+  end
 end
