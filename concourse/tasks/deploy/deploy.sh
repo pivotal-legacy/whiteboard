b73fff72ad0cff909c02f9410533239d2142cc07
Peter Iskandar <piskandar@pivotal.io>
2017-09-06 10:32:38 -0400

    updated deploy scripts to add variables for production
    Signed-off-by: Drew Archibald <darchibald@pivotal.io>

--- concourse/tasks/deploy/deploy.sh
+++ concourse/tasks/deploy/deploy.sh
@@ -29,7 +29,7 @@ cd maintenance-mode
 bundle install
 
 cf push -f manifest.yml
-start-maintenance whiteboard $CF_DOMAIN whiteboard-staging
+start-maintenance whiteboard $CF_DOMAIN whiteboard-$ENVIRONMENT
 
 cd ../whiteboard
 
@@ -38,13 +38,25 @@ cf set-env whiteboard OKTA_CERT_FINGERPRINT $OKTA_CERT_FINGERPRINT
 cf set-env whiteboard IP_WHITELIST $IP_WHITELIST
 cf set-env whiteboard SENTRY_DSN $SENTRY_DSN
 
+if [ "$ENVIRONMENT" == "production" ]; then
+    cf set-env whiteboard EXCEPTIONAL_API_KEY $EXCEPTIONAL_API_KEY
+    cf set-env whiteboard GOOGLE_CLIENT_ID $GOOGLE_CLIENT_ID
+    cf set-env whiteboard GOOGLE_CLIENT_SECRET $GOOGLE_CLIENT_SECRET
+    cf set-env whiteboard NEWRELIC_APP_NAME $NEWRELIC_APP_NAME
+    cf set-env whiteboard NEWRELIC_LICENSE $NEWRELIC_LICENSE
+    cf set-env whiteboard WORDPRESS_BLOG_HOST $WORDPRESS_BLOG_HOST
+    cf set-env whiteboard WORDPRESS_PASSWORD $WORDPRESS_PASSWORD
+    cf set-env whiteboard WORDPRESS_USER $WORDPRESS_USER
+    cf set-env whiteboard WORDPRESS_XMLRPC_ENDPOINT_PATH $WORDPRESS_XMLRPC_ENDPOINT_PATH
+fi
+
 bundle install
 bundle exec rake SPACE=$CF_SPACE cf:deploy:$ENVIRONMENT
 
 STATUS=$?
 echo $STATUS
 if [ $STATUS -eq 0 ];then
-    stop-maintenance whiteboard $CF_DOMAIN whiteboard-staging
+    stop-maintenance whiteboard $CF_DOMAIN whiteboard-$ENVIRONMENT
 else
     exit $STATUS
 fi
