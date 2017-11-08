be86a3f0b60cc9787239929b116d907eb0b8594b
Peter Iskandar <piskandar@pivotal.io>
2017-11-08 16:43:39 -0500

    added env values to prod pipeline
    Signed-off-by: Boris Chan <borchan@pivotal.io>

--- concourse/tasks/map-routes/map-routes.sh
+++ concourse/tasks/map-routes/map-routes.sh
@@ -7,6 +7,6 @@ sudo dpkg -i cf-cli.deb
 cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE -a $CF_API_ENDPOINT
 
 cf unmap-route maintenance-mode $CF_DOMAIN -n $WHITEBOARD_HOSTNAME
-cf unmap-route whiteboard $CF_DOMAIN -n $TEMP_WHITEBOARD_HOSTNAME
+cf unmap-route whiteboard $CF_TEMP_DOMAIN -n $TEMP_WHITEBOARD_HOSTNAME
 cf map-route whiteboard $CF_DOMAIN -n $WHITEBOARD_HOSTNAME
 
