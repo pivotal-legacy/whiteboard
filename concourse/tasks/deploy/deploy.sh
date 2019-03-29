70d36e892523a80accc291d1427e0cbe30e0c4e4
Ian Fisher <loktar@gmail.com>
2019-03-29 16:25:20 -0700

    Use cf push to deploy instead of evaporator
    Co-authored-by: Colin O'Byrne <cobyrne@pivotal.io>
    Co-authored-by: Ian Fisher <loktar@gmail.com>

--- concourse/tasks/deploy/deploy.sh
+++ concourse/tasks/deploy/deploy.sh
@@ -6,6 +6,7 @@ cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE -a $CF_API_ENDP
 
 cd whiteboard
 
+cf set-env whiteboard GIT_SHA `git rev-parse HEAD`
 cf set-env whiteboard OKTA_SSO_TARGET_URL $OKTA_SSO_TARGET_URL
 cf set-env whiteboard OKTA_CERT_FINGERPRINT $OKTA_CERT_FINGERPRINT
 cf set-env whiteboard IP_WHITELIST $IP_WHITELIST
@@ -26,4 +27,4 @@ fi
 bundle install
 
 #takes hostname from config/cf-{ENVIRONMENT}.yml manifest
-bundle exec rake SPACE=$CF_SPACE cf:deploy:$ENVIRONMENT
+cf push -f concourse/manifests/$ENVIRONMENT.yml
