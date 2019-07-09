d88fc56fcc4340d7a7092186546a0f5475da391d
Ryan Lam <rlam@pivotal.io>
2019-07-09 15:26:07 -0700

    Added monkey-patch to suppress Capybara normalize_whitespace warning.
    Co-authored-by: Ryan Lam <rlam@pivotal.io>
    Co-authored-by: Alex Lobascio <alobascio@pivotal.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,3 +38,19 @@ RSpec.configure do |config|
     page.find(:css, 'body').click
   end
 end
+
+module Capybara
+  module Helpers
+    class << self
+
+      alias_method :normalize_whitespace_with_warning, :normalize_whitespace
+
+      def normalize_whitespace(*args)
+        silence_warnings do
+          normalize_whitespace_with_warning(*args)
+        end
+      end
+
+    end
+  end
+end
