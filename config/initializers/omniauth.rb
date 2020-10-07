ff8141db0a03f00d069c2e0489b194d50a7859c0
Colin O'Byrne <cobyrne@pivotal.io>
2020-10-07 12:14:57 -0700

    Fix the feature specs
    If we skip all the omniauth stuff then no sso paths get configured and
    the auth tests are sad. But we don't need real values because we don't
    auth that way in test.
    
    Co-authored-by: Colin O'Byrne <cobyrne@pivotal.io>
    Co-authored-by: Andrew Hall <ahall@pivotal.io>

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,16 @@
 require 'omniauth'
 
-unless Rails.env.test?
-  idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
-  idp_metadata = idp_metadata_parser.parse_remote_to_hash(ENV["IDP_METADATA_XML_URL"])
-
-  Rails.application.config.middleware.use OmniAuth::Strategies::SAML,
-                                          idp_metadata.merge(
-                                              :issuer => ENV["WS1_AUDIENCE"]
-                                          )
+def get_idp_metadata
+  if Rails.env.test?
+    {}
+  else
+    idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
+    idp_metadata_parser.parse_remote_to_hash(ENV["IDP_METADATA_XML_URL"])
+  end
 end
+
+
+Rails.application.config.middleware.use OmniAuth::Strategies::SAML,
+                                        get_idp_metadata.merge(
+                                            :issuer => ENV["WS1_AUDIENCE"]
+                                        )
