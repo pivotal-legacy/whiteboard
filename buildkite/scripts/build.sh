6ebeadd202f51d937f512fbd23b4c26313351df1
Peter Iskandar <piskandar@pivotal.io>
2020-12-15 13:33:19 -0500

    added start of whiteboard staging pipeline

---
+++ buildkite/scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e -x
+export IDP_METADATA_XML_URL=$TEST_IDP_METADATA_XML_URL
+
+pushd whiteboard
+#    before install
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start
+    gem install bundler -v 1.10.6
+
+#    install
+    bundle install --with development
+
+    /etc/init.d/mysql start
+
+#    before script
+    bundle exec rake db:setup
+
+#    script
+    xvfb-run -a bundle exec rspec
+
+popd
